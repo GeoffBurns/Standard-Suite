01d5bb576e5c0aa525df828606635bcd56b7468a
Geoff Burns <Geoffrey.Peter.Burns@gmail.com>
2021-12-13 09:06:25 +1100

    remove extra values
    remove extra values

--- Sources/Standard-Suite/Image/StandardCardImageRepository.swift
+++ Sources/Standard-Suite/Image/StandardCardImageRepository.swift
@@ -19,14 +19,32 @@ public class StandardCardImageRepository : ICardImageRepository
          PlayingCard.Suite.spades,
          PlayingCard.Suite.hearts,
          PlayingCard.Suite.diamonds,
-         PlayingCard.Suite.clubs,
+         PlayingCard.Suite.clubs
+        ]
+    public static let standardValues =
+        [
+            2.cardValue,
+            3.cardValue,
+            4.cardValue,
+            5.cardValue,
+            6.cardValue,
+            7.cardValue,
+            8.cardValue,
+            9.cardValue,
+            10.cardValue,
+            CardName.ace.cardValue,
+            CardName.jack.cardValue,
+            CardName.queen.cardValue,
+            CardName.king.cardValue
         ]
     public func image(card: PlayingCard) -> Image?
-    {
+    { 
         guard StandardCardImageRepository.standardSuites.contains(card.suite) else {
             return nil
         }
-        
+        guard StandardCardImageRepository.standardValues.contains(card.value) else {
+            return nil
+        }
         return Image(card.imageName, bundle: Bundle.module) 
     }
     public func register()
